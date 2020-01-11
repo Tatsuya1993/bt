2e6eb0c3409be440b76012217771183066426498
Tatsuya1993 <vividred0121@gmail.com>
2020-01-11 03:20:31 +0000

    config assets.compile

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,6 @@ Rails.application.initialize!
 # Pagination
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&lt 前へ'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = '次へ &gt'
+
+# heroku
+config.assets.compile = true
